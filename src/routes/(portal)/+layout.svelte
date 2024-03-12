f79b5e93fccbc008d05d4dd0825dbc7eb6093aa4
Enrico Saez <enricoricsaez@gmail.com>
2024-03-12 15:45:56 -0300

    Dark mode implementado

--- src/routes/(portal)/+layout.svelte
+++ src/routes/(portal)/+layout.svelte
@@ -1,23 +1,75 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { darkMode } from '$lib/scripts/stores/darkMode';
+
+	function toggleLightMode() {
+		$darkMode = false;
+		document.body.classList.remove('dark');
+	}
+	function toggleDarkMode() {
+		$darkMode = true;
+		document.body.classList.add('dark');
+	}
 </script>
 
-<nav class="fixed top-0 w-full bg-white">
-	<div class="mx-auto flex h-14 max-w-7xl items-center justify-between px-4">
-		<p>Logo Smart Campus</p>
-		<div class="flex items-center space-x-2">
-			<p class="font-medium">Nuncio</p>
-			<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
-				><path
-					fill="currentColor"
-					d="M5.85 17.1q1.275-.975 2.85-1.537T12 15q1.725 0 3.3.563t2.85 1.537q.875-1.025 1.363-2.325T20 12q0-3.325-2.337-5.663T12 4Q8.675 4 6.337 6.338T4 12q0 1.475.488 2.775T5.85 17.1M12 13q-1.475 0-2.488-1.012T8.5 9.5q0-1.475 1.013-2.488T12 6q1.475 0 2.488 1.013T15.5 9.5q0 1.475-1.012 2.488T12 13m0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22"
-				/></svg
-			>
+<nav class="fixed top-0 z-10 w-full bg-white dark:bg-neutral-800">
+	<div class="mx-auto flex h-14 max-w-7xl items-center justify-between px-6">
+		<p class="text-neutral-900 dark:text-neutral-100">Logo Smart Campus</p>
+
+		<div class="flex space-x-12">
+			<div class="flex space-x-3">
+				<button
+					class="cursor-default rounded-full p-1 transition dark:hover:cursor-pointer dark:hover:bg-neutral-600"
+					on:click={toggleLightMode}
+				>
+					<svg
+						class="text-orange-400 dark:text-neutral-700"
+						xmlns="http://www.w3.org/2000/svg"
+						width="28"
+						height="28"
+						viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							d="M12 17q-2.075 0-3.537-1.463T7 12q0-2.075 1.463-3.537T12 7q2.075 0 3.538 1.463T17 12q0 2.075-1.463 3.538T12 17M2 13q-.425 0-.712-.288T1 12q0-.425.288-.712T2 11h2q.425 0 .713.288T5 12q0 .425-.288.713T4 13zm18 0q-.425 0-.712-.288T19 12q0-.425.288-.712T20 11h2q.425 0 .713.288T23 12q0 .425-.288.713T22 13zm-8-8q-.425 0-.712-.288T11 4V2q0-.425.288-.712T12 1q.425 0 .713.288T13 2v2q0 .425-.288.713T12 5m0 18q-.425 0-.712-.288T11 22v-2q0-.425.288-.712T12 19q.425 0 .713.288T13 20v2q0 .425-.288.713T12 23M5.65 7.05L4.575 6q-.3-.275-.288-.7t.288-.725q.3-.3.725-.3t.7.3L7.05 5.65q.275.3.275.7t-.275.7q-.275.3-.687.288T5.65 7.05M18 19.425l-1.05-1.075q-.275-.3-.275-.712t.275-.688q.275-.3.688-.287t.712.287L19.425 18q.3.275.288.7t-.288.725q-.3.3-.725.3t-.7-.3M16.95 7.05q-.3-.275-.288-.687t.288-.713L18 4.575q.275-.3.7-.288t.725.288q.3.3.3.725t-.3.7L18.35 7.05q-.3.275-.7.275t-.7-.275M4.575 19.425q-.3-.3-.3-.725t.3-.7l1.075-1.05q.3-.275.712-.275t.688.275q.3.275.288.688t-.288.712L6 19.425q-.275.3-.7.288t-.725-.288"
+						/></svg
+					>
+				</button>
+				<button
+					class="rounded-full p-1 transition hover:bg-neutral-200 dark:cursor-default dark:hover:cursor-default dark:hover:bg-transparent"
+					on:click={toggleDarkMode}
+				>
+					<svg
+						class="text-neutral-400 dark:text-indigo-400"
+						xmlns="http://www.w3.org/2000/svg"
+						width="28"
+						height="28"
+						viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							d="M9.5 22q-.875 0-1.75-.175T6.075 21.3q-.275-.125-.45-.375t-.175-.55q0-.225.1-.425t.3-.35q1.75-1.375 2.7-3.375T9.5 12q0-2.225-.962-4.213T5.825 4.4q-.175-.15-.275-.35t-.1-.425q0-.3.163-.55T6.05 2.7q.825-.35 1.7-.525T9.5 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T19.5 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T9.5 22"
+						/></svg
+					>
+				</button>
+			</div>
+			<div class="flex items-center space-x-2">
+				<p class="font-medium text-neutral-900 dark:text-neutral-100">Nuncio</p>
+				<svg
+					class="text-neutral-900 dark:text-neutral-100"
+					xmlns="http://www.w3.org/2000/svg"
+					width="28"
+					height="28"
+					viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="M5.85 17.1q1.275-.975 2.85-1.537T12 15q1.725 0 3.3.563t2.85 1.537q.875-1.025 1.363-2.325T20 12q0-3.325-2.337-5.663T12 4Q8.675 4 6.337 6.338T4 12q0 1.475.488 2.775T5.85 17.1M12 13q-1.475 0-2.488-1.012T8.5 9.5q0-1.475 1.013-2.488T12 6q1.475 0 2.488 1.013T15.5 9.5q0 1.475-1.012 2.488T12 13m0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22"
+					/></svg
+				>
+			</div>
 		</div>
 	</div>
 </nav>
-<div class="flex h-screen divide-x divide-neutral-300">
-	<nav class="fixed top-14 h-full w-56 bg-neutral-100 px-6 py-8">
+<div class="flex h-screen divide-x divide-neutral-300 dark:divide-neutral-700">
+	<nav class="fixed top-14 h-full w-56 bg-neutral-100 px-6 py-8 dark:bg-[#121212]">
 		<div class="flex flex-col space-y-3">
 			<a
 				class="block text-nowrap rounded-full px-5 py-2 text-[15px] transition"
@@ -39,7 +91,7 @@
 			>
 		</div>
 	</nav>
-	<div class="ml-56 w-full overflow-y-auto bg-neutral-100 p-8 py-[5.5rem]">
+	<div class="ml-56 w-full overflow-y-auto bg-neutral-100 p-8 py-[5.5rem] dark:bg-[#121212]">
 		<div class="h-full">
 			<slot />
 		</div>
--- src/routes/(portal)/reservatorios/+page.svelte
+++ src/routes/(portal)/reservatorios/+page.svelte
@@ -2,10 +2,9 @@
 </script>
 
 <div class="grid grid-cols-4 justify-items-center gap-y-10">
-	<div class="flex h-48 w-48 flex-col justify-end overflow-hidden rounded-xl bg-white">
-		<div class="h-10 w-full bg-cyan-300"></div>
-	</div>
-	<div class="relative flex h-48 w-48 flex-col justify-end overflow-hidden rounded-xl bg-white">
+	<div
+		class="relative flex h-48 w-48 flex-col justify-end overflow-hidden rounded-xl bg-white dark:bg-neutral-800"
+	>
 		<svg xmlns="http://www.w3.org/2000/svg" class="w-full" height="20">
 			<g class="transition-transform duration-500">
 				<path
@@ -17,19 +16,28 @@
 		<div class="h-20 w-full bg-cyan-300"></div>
 		<div class="absolute bottom-2 right-2 text-right text-cyan-950">
 			<h1 class="text-3xl font-semibold">500L</h1>
-			<p class="font-medium">Reservatório 2</p>
+			<p class="font-medium">Reservatório 1</p>
 		</div>
 	</div>
-	<div class="h-48 w-48 rounded-lg bg-white"></div>
-	<div class="h-48 w-48 rounded-lg bg-white"></div>
-	<div class="h-48 w-48 rounded-lg bg-white"></div>
-	<div class="h-48 w-48 rounded-lg bg-white"></div>
-	<div class="h-48 w-48 rounded-lg bg-white"></div>
-	<div class="h-48 w-48 rounded-lg bg-white"></div>
-	<div class="h-48 w-48 rounded-lg bg-white"></div>
-	<div class="h-48 w-48 rounded-lg bg-white"></div>
-	<div class="h-48 w-48 rounded-lg bg-white"></div>
-	<div class="h-48 w-48 rounded-lg bg-white"></div>
+	{#each Array(11) as _, i}
+		<div
+			class="relative flex h-48 w-48 flex-col justify-end overflow-hidden rounded-xl bg-white dark:bg-neutral-800"
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" class="w-full" height="20">
+				<g class="transition-transform duration-500">
+					<path
+						class="water fill-cyan-300"
+						d="M420 20.0047C441.5 19.6047 458.8 17.5047 471.1 15.5047C484.5 13.3047 497.6 10.3047 498.4 10.1047C514 6.50474 518 4.70474 528.5 2.70474C535.6 1.40474 546.4 -0.0952561 560 0.00474393V20.0047H420ZM420 20.0047C398.5 19.6047 381.2 17.5047 368.9 15.5047C355.5 13.3047 342.4 10.3047 341.6 10.1047C326 6.50474 322 4.70474 311.5 2.70474C304.3 1.40474 293.6 -0.0952561 280 0.00474393V20.0047H420ZM140 20.0047C118.5 19.6047 101.2 17.5047 88.9 15.5047C75.5 13.3047 62.4 10.3047 61.6 10.1047C46 6.50474 42 4.70474 31.5 2.70474C24.3 1.40474 13.6 -0.0952561 0 0.00474393V20.0047H140ZM140 20.0047C161.5 19.6047 178.8 17.5047 191.1 15.5047C204.5 13.3047 217.6 10.3047 218.4 10.1047C234 6.50474 238 4.70474 248.5 2.70474C255.6 1.40474 266.4 -0.0952561 280 0.00474393V20.0047H140Z"
+					/>
+				</g>
+			</svg>
+			<div class="h-20 w-full bg-cyan-300"></div>
+			<div class="absolute bottom-2 right-2 text-right text-cyan-950">
+				<h1 class="text-3xl font-semibold">500L</h1>
+				<p class="font-medium">Reservatório {i + 2}</p>
+			</div>
+		</div>
+	{/each}
 </div>
 
 <style>
