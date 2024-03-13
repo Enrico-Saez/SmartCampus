05b636a5bc53fd3a77383f8e477c63cdacceaa13
Enrico Saez <enricoricsaez@gmail.com>
2024-03-13 15:44:28 -0300

    Update página de login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,11 @@
 
 <div class="flex h-screen items-center justify-center bg-[#0066FF]">
 	<div>
-		<h1 class="text-6xl font-bold text-white">SmartCampus</h1>
+		<h1 class="text-6xl font-bold text-white">SmartCampus Mauá</h1>
 		<div class="mt-20 flex justify-center">
 			<a
 				href="/reservatorios"
-				class="mx-auto flex items-center space-x-5 rounded-full bg-black/30 px-7 py-4"
+				class="mx-auto flex items-center space-x-5 rounded-full bg-black/30 px-7 py-4 transition duration-150 ease-in hover:bg-black/50"
 			>
 				<p class="text-center text-[0.825rem] uppercase tracking-wider text-white">ENTRAR COM</p>
 				<img class="h-[1.3rem]" src="/images/Microsoft_365.svg" alt="" /></a
