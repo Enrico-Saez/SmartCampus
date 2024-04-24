19f9dc497070c3757a615ff9274eb1248e772313
Felipe Nelli Martins <Fn.martins@outlook.com>
2024-04-24 01:44:00 -0300

    Logout Firebase + Microsoft

--- src/lib/scripts/auth.ts
+++ src/lib/scripts/auth.ts
@@ -1,4 +1,5 @@
 import { OAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { PUBLIC_AZURE_TENANT, PUBLIC_AZURE_APPID } from '$env/static/public'
 import { auth } from "./firebase-portal";
 import { goto } from "$app/navigation";
 
@@ -6,7 +7,7 @@ export const authHandlers = {
     loginWithMicrosoft: async () => {
         const provider = new OAuthProvider('microsoft.com');
         const credential = await signInWithPopup(auth, provider);
-        
+       
         const idToken = await credential.user.getIdToken()
     
         await fetch("api/login", {
@@ -17,11 +18,12 @@ export const authHandlers = {
           },
           body: JSON.stringify({ idToken }),
         });
-        goto('/reservatorios')
+        goto("/reservatorios")
     },
     
     logOutWithMicrosoft: async () => {
         await signOut(auth);
         await fetch("/api/login", { method: "DELETE" });
+        window.location.href = "https://login.microsoftonline.com/" + PUBLIC_AZURE_TENANT + "/oauth2/v2.0/logout?" + PUBLIC_AZURE_APPID + "&post_logout_redirect_uri=http%3A%2F%2Flocalhost%3A5173";
     }
 }
--- src/routes/(portal)/+layout.svelte
+++ src/routes/(portal)/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { authHandlers } from '$lib/scripts/auth.js';
 	export let data;
 	let displayName = data.displayName;
 	function toggleLightMode() {
@@ -60,9 +61,8 @@
 				</button>
 			</div>
 			<div class="flex items-center space-x-2">
-<<<<<<< Updated upstream
-				<p class="font-medium text-neutral-900 dark:text-neutral-100">Nuncio</p>
-				<button class="bg-primary flex items-center space-x-1 rounded-full px-3 py-1 text-black">
+				<p class="font-medium text-neutral-900 dark:text-neutral-100">{displayName}</p>
+				<button class="bg-primary flex items-center space-x-1 rounded-full px-3 py-1 text-black" on:click={authHandlers.logOutWithMicrosoft}>
 					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
 						><path
 							fill="currentColor"
@@ -74,20 +74,6 @@
 					>
 					<span class="text-[0.825rem] font-semibold tracking-wide">Sair</span>
 				</button>
-=======
-				<p class="font-medium text-neutral-900 dark:text-neutral-100">{displayName}</p>
-				<svg
-					class="text-neutral-900 dark:text-neutral-100"
-					xmlns="http://www.w3.org/2000/svg"
-					width="28"
-					height="28"
-					viewBox="0 0 24 24"
-					><path
-						fill="currentColor"
-						d="M5.85 17.1q1.275-.975 2.85-1.537T12 15q1.725 0 3.3.563t2.85 1.537q.875-1.025 1.363-2.325T20 12q0-3.325-2.337-5.663T12 4Q8.675 4 6.337 6.338T4 12q0 1.475.488 2.775T5.85 17.1M12 13q-1.475 0-2.488-1.012T8.5 9.5q0-1.475 1.013-2.488T12 6q1.475 0 2.488 1.013T15.5 9.5q0 1.475-1.012 2.488T12 13m0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22"
-					/></svg
-				>
->>>>>>> Stashed changes
 			</div>
 		</div>
 	</div>
