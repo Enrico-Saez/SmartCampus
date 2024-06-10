180986183eb3c15753ae9f60908c087dcd1b9de9
Enrico Saez <enricoricsaez@gmail.com>
2024-06-10 19:42:17 -0300

    Ajustes

--- src/routes/(portal)/+layout.svelte
+++ src/routes/(portal)/+layout.svelte
@@ -54,7 +54,11 @@
 					<img class="hidden h-8 dark:block" src="/images/logo_negative.svg" alt="EcoVision" />
 				</div>
 				<p class="font-outfit text-2xl font-medium text-primary">
-					Eco<span class="text-tertiary dark:text-white">Vision</span>{#if userAdmin}<span class="font-outfit text-xl font-normal text-tertiary dark:text-white"> | admin</span>{/if}
+					Eco<span class="text-tertiary dark:text-white">Vision</span>{#if userAdmin}<span
+							class="ml-1.5 font-outfit text-xl font-normal text-tertiary dark:text-white"
+						>
+							| admin</span
+						>{/if}
 				</p>
 			</div>
 			<div class="flex space-x-3">
@@ -188,22 +192,22 @@
 				</div>
 			</div>
 			{#if userAdmin}
-			<div>
-				<a
-					class="flex items-center space-x-2 text-nowrap rounded-full px-3 py-2.5 text-[0.825rem] tracking-wide transition"
-					class:button-pressed={$page.route.id?.match(/configuracoes/g)}
-					class:button-unpressed={!$page.route.id?.match(/configuracoes/g)}
-					href="/configuracoes"
-				>
-					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-						><path
-							fill="currentColor"
-							d="M19.5 12c0-.23-.01-.45-.03-.68l1.86-1.41c.4-.3.51-.86.26-1.3l-1.87-3.23a.987.987 0 0 0-1.25-.42l-2.15.91c-.37-.26-.76-.49-1.17-.68l-.29-2.31c-.06-.5-.49-.88-.99-.88h-3.73c-.51 0-.94.38-1 .88l-.29 2.31c-.41.19-.8.42-1.17.68l-2.15-.91c-.46-.2-1-.02-1.25.42L2.41 8.62c-.25.44-.14.99.26 1.3l1.86 1.41a7.343 7.343 0 0 0 0 1.35l-1.86 1.41c-.4.3-.51.86-.26 1.3l1.87 3.23c.25.44.79.62 1.25.42l2.15-.91c.37.26.76.49 1.17.68l.29 2.31c.06.5.49.88.99.88h3.73c.5 0 .93-.38.99-.88l.29-2.31c.41-.19.8-.42 1.17-.68l2.15.91c.46.2 1 .02 1.25-.42l1.87-3.23c.25-.44.14-.99-.26-1.3l-1.86-1.41c.03-.23.04-.45.04-.68m-7.46 3.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5s3.5 1.57 3.5 3.5s-1.57 3.5-3.5 3.5"
-						/></svg
+				<div>
+					<a
+						class="flex items-center space-x-2 text-nowrap rounded-full px-3 py-2.5 text-[0.825rem] tracking-wide transition"
+						class:button-pressed={$page.route.id?.match(/configuracoes/g)}
+						class:button-unpressed={!$page.route.id?.match(/configuracoes/g)}
+						href="/configuracoes"
 					>
-					<span>CONFIGURAÇÕES</span></a
-				>
-			</div>
+						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+							><path
+								fill="currentColor"
+								d="M19.5 12c0-.23-.01-.45-.03-.68l1.86-1.41c.4-.3.51-.86.26-1.3l-1.87-3.23a.987.987 0 0 0-1.25-.42l-2.15.91c-.37-.26-.76-.49-1.17-.68l-.29-2.31c-.06-.5-.49-.88-.99-.88h-3.73c-.51 0-.94.38-1 .88l-.29 2.31c-.41.19-.8.42-1.17.68l-2.15-.91c-.46-.2-1-.02-1.25.42L2.41 8.62c-.25.44-.14.99.26 1.3l1.86 1.41a7.343 7.343 0 0 0 0 1.35l-1.86 1.41c-.4.3-.51.86-.26 1.3l1.87 3.23c.25.44.79.62 1.25.42l2.15-.91c.37.26.76.49 1.17.68l.29 2.31c.06.5.49.88.99.88h3.73c.5 0 .93-.38.99-.88l.29-2.31c.41-.19.8-.42 1.17-.68l2.15.91c.46.2 1 .02 1.25-.42l1.87-3.23c.25-.44.14-.99-.26-1.3l-1.86-1.41c.03-.23.04-.45.04-.68m-7.46 3.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5s3.5 1.57 3.5 3.5s-1.57 3.5-3.5 3.5"
+							/></svg
+						>
+						<span>CONFIGURAÇÕES</span></a
+					>
+				</div>
 			{/if}
 		</div>
 	</nav>
--- src/routes/(portal)/lampadas/+page.svelte
+++ src/routes/(portal)/lampadas/+page.svelte
@@ -16,33 +16,59 @@
 		>
 			<div class="relative">
 				<svg
-					class="relative z-20 ml-2 mt-3 text-neutral-900 dark:text-slate-200 fill-none"
+					class="relative z-10 mx-auto mt-5 fill-none text-neutral-500 shadow-yellow-300 drop-shadow-2xl dark:text-neutral-700"
 					xmlns="http://www.w3.org/2000/svg"
 					width="90"
 					height="90"
 					viewBox="0 0 24 24"
 					><path
-						d="M12 4a5.51 5.51 0 0 0-5.5 5.5c0 2.47 1.49 3.89 2.35 4.5h6.3c.86-.61 2.35-2.03 2.35-4.5C17.5 6.47 15.03 4 12 4" opacity=".3"/><path fill="currentColor" d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2m-4-5h8v2H8zm4-15C7.86 2 4.5 5.36 4.5 9.5c0 3.82 2.66 5.86 3.77 6.5h7.46c1.11-.64 3.77-2.68 3.77-6.5C19.5 5.36 16.14 2 12 2m3.15 12h-6.3c-.86-.61-2.35-2.03-2.35-4.5C6.5 6.47 8.97 4 12 4s5.5 2.47 5.5 5.5c0 2.47-1.49 3.89-2.35 4.5"
+						d="M12 4a5.51 5.51 0 0 0-5.5 5.5c0 2.47 1.49 3.89 2.35 4.5h6.3c.86-.61 2.35-2.03 2.35-4.5C17.5 6.47 15.03 4 12 4"
+						opacity=".3"
+					/><path
+						fill="currentColor"
+						d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2m-4-5h8v2H8zm4-15C7.86 2 4.5 5.36 4.5 9.5c0 3.82 2.66 5.86 3.77 6.5h7.46c1.11-.64 3.77-2.68 3.77-6.5C19.5 5.36 16.14 2 12 2m3.15 12h-6.3c-.86-.61-2.35-2.03-2.35-4.5C6.5 6.47 8.97 4 12 4s5.5 2.47 5.5 5.5c0 2.47-1.49 3.89-2.35 4.5"
 					/></svg
 				>
 
-				<div
-					class="lampada absolute left-[21%] top-[26%] h-6 w-7 rounded-3xl bg-[#ffe770]"
-				></div>
+				<div class="lampada absolute left-[5.1rem] top-6 h-6 w-7 rounded-3xl bg-yellow-300"></div>
 			</div>
-			<div
-				class="absolute bottom-0 right-0 z-20 hidden h-[4.15rem] w-full bg-black/60 dark:block"
-			></div>
 			<div class="absolute bottom-2 right-3 z-30 text-right text-yellow-950 dark:text-yellow-100">
 				<h1 class="text-xl font-semibold">
 					<span class="mr-2 text-base font-medium text-yellow-600 dark:text-yellow-300"
-						>Luz acesa</span
+						>Luz acesa há</span
 					>50h
 				</h1>
 				<p class="text-sm font-medium">Lâmpada {i + 1}</p>
 			</div>
 		</div>
 	{/each}
+	<div
+		class="animate-fade-in relative h-48 w-48 overflow-hidden rounded-xl bg-white dark:bg-neutral-900 dark:shadow-black"
+		style="box-shadow: 8px 8px 25px rgba(0,0,0,.2)"
+	>
+		<div class="relative">
+			<svg
+				class="relative z-10 mx-auto mt-5 fill-none text-neutral-500 shadow-yellow-300 drop-shadow-2xl dark:text-neutral-700"
+				xmlns="http://www.w3.org/2000/svg"
+				width="90"
+				height="90"
+				viewBox="0 0 24 24"
+				><path
+					d="M12 4a5.51 5.51 0 0 0-5.5 5.5c0 2.47 1.49 3.89 2.35 4.5h6.3c.86-.61 2.35-2.03 2.35-4.5C17.5 6.47 15.03 4 12 4"
+					opacity=".3"
+				/><path
+					fill="currentColor"
+					d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2m-4-5h8v2H8zm4-15C7.86 2 4.5 5.36 4.5 9.5c0 3.82 2.66 5.86 3.77 6.5h7.46c1.11-.64 3.77-2.68 3.77-6.5C19.5 5.36 16.14 2 12 2m3.15 12h-6.3c-.86-.61-2.35-2.03-2.35-4.5C6.5 6.47 8.97 4 12 4s5.5 2.47 5.5 5.5c0 2.47-1.49 3.89-2.35 4.5"
+				/></svg
+			>
+		</div>
+		<div class="absolute bottom-2 right-3 z-30 text-right text-yellow-950 dark:text-yellow-100">
+			<h1 class="text-xl font-semibold">
+				<span class="text-base font-medium text-yellow-600 dark:text-yellow-300">Luz apagada</span>
+			</h1>
+			<p class="text-sm font-medium">Lâmpada 11</p>
+		</div>
+	</div>
 </div>
 
 <style>
@@ -50,7 +76,7 @@
 		box-shadow:
 			0 0 0 10px #ffe770,
 			0 0 300px 10px #ffcd00;
-		animation: glow 3s infinite ease-in-out;
+		animation: glow 4s infinite ease-in-out;
 	}
 
 	@keyframes glow {
