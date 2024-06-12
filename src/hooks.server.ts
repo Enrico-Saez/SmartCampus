f0e0291a455c3de590f088c48c14c748c87e68d5
Felipe Nelli Martins <Fn.martins@outlook.com>
2024-06-12 19:37:03 -0300

    Removed some telegram code

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,4 @@
 import { adminAuthPortal, adminDBPortal } from '$lib/server/firebase-admin-portal';
-import { startProcess } from '$lib/server/telegram';
 import { type Handle, redirect } from '@sveltejs/kit';
 
 
@@ -11,11 +10,6 @@ async function getUserData(uid: string | null) {
   }
   return data;
 }
- 
-if (import.meta.env.SSR) {
-	setInterval(startProcess, 15 * 60 * 1000);
-  }
-
 
 export const handle = (async ({ event, resolve }) => {
 	const sessionCookie = event.cookies.get('__session');
@@ -45,7 +39,7 @@ export const handle = (async ({ event, resolve }) => {
 		}		
 	}
 	if (!event.locals.userID) {
-		if (!event.url.pathname.endsWith("/") && !event.url.pathname.startsWith("/api/login") && event.request.headers.get("X-Sveltekit-Action") == "true"){
+		if ((!event.url.pathname.endsWith("/") && !event.url.pathname.startsWith("/api/login") && event.request.headers.get("X-Sveltekit-Action") == "true") && !event.url.pathname.startsWith("/api/telegramAlert")){
 			throw redirect(303, '/');
 		}
 	}
